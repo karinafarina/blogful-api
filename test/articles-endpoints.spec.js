610a94f9129141fcee846eadcb13fe3c1fedef11
Karina Gaulin <karina@domandtom.com>
2019-10-22 07:15:20 -0400

    All tests passing

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -172,7 +172,15 @@ describe('Articles Endpoints', function() {
     })
   })
 
-  describe.only(`DELETE /articles/:article_id`, () => {
+  describe(`DELETE /articles/:article_id`, () => {
+    context(`Given no articles`, () => {
+      it(`responds with 404`, () => {
+        const articleId = 123456
+          return supertest(app)
+            .delete(`/articles/${articleId}`)
+            .expect(404, { error: { message: `Article doesn't exist` } })
+        })
+    })
     context('Given there are articles in the database', () => {
       const testArticles = makeArticlesArray()
 
